0c3507fc2c992acb1ea2774d5613e7f1eb5801bd
Cheikh-Kanteye <cheikhkanteye.contact@gmail.com>
2024-07-22 21:27:59 +0000

    m

--- components/CommandCard.tsx
+++ components/CommandCard.tsx
@@ -13,7 +13,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 const CommandCard = () => {
   return (
-    <Card className="max-w-[20rem] w-full">
+    <Card className="max-w-screen-sm mx-auto w-full">
       <CardHeader>
         <CardTitle>Commande</CardTitle>
         <CardDescription>
